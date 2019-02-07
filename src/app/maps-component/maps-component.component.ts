15cb3bec7f4e9d03c261398ab75a9c67e4cc59b0
Guillaume SICARD <gsicard@enssat.fr>
2019-02-07 15:35:46 +0100

    add marker in map

--- src/app/maps-component/maps-component.component.ts
+++ src/app/maps-component/maps-component.component.ts
@@ -1,26 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import { latLng, tileLayer, icon, marker, map } from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-maps-component',
   templateUrl: './maps-component.component.html',
   styleUrls: ['./maps-component.component.css']
 })
+
 export class MapsComponentComponent implements OnInit {
 
-  constructor() { }
+  waypoints:JSON
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    const myMap = map('map').setView([39.162911, -101.723353], 4);
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; OpenStreetMap contributors' }).addTo(myMap);
+    const markerIcon = icon({
+      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+    });
+    this.http.get('./assets/chapters.json', { responseType: 'json' }).subscribe(data => {
+      this.waypoints = data['Waypoints'];
+      for (var i = 0; i < this.waypoints.length; i++){
+          var current_waypoint = this.waypoints[i];
+          marker([current_waypoint.lat, current_waypoint.lng ], {icon: markerIcon}).bindPopup(current_waypoint.label).addTo(myMap);
+      }
+    });
   }
-
+  /*
   options = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
       })
     ],
-    zoom: 5,
-    center: latLng([ 46.879966, -121.726909 ])
+    zoom: 4,
+    center: latLng([ 39.162911, -101.723353 ])
   };
-
+  */
 }
